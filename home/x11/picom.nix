8e18de26ff309071458d5451d49bfdecaa4df69e
Maxim <maxim.payeur@epita.fr>
2024-12-02 12:57:40 +0100

    update: some picom changes

--- home/x11/picom.nix
+++ home/x11/picom.nix
@@ -6,6 +6,15 @@
     fadeExclude = [
       "window_type *= 'menu'"
     ];
+
+    opacityRules = [
+      "90:class_g = 'Alacritty' && focused"
+      "65:class_g = 'Alacritty' && !focused"
+    ];
+
     shadow = true;
+    settings = {
+      corner-radius = 10;
+    };
   };
 }
